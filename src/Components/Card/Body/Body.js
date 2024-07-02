3996f26182489013bdb444a567553e02342969bf
TSM97 <Gtsamadias@hotmail.com>
2024-07-02 22:46:39 +0300

    Fix(Fast fix cause API result changed)

--- src/Components/Card/Body/Body.js
+++ src/Components/Card/Body/Body.js
@@ -1,23 +1,26 @@
-import { useEffect ,useState} from "react";
+import { useEffect, useState } from "react";
 import Tab from "../Tab/Tab";
+import { random } from "lodash";
 
-const chooseImage = (Ids,id) => Ids.find(element => element.id===id).image
-const shuffle= arr => [...arr].sort(()=> Math.random() - 0.5);
+const chooseImage = (Ids, id) => Ids.find(element => element.id === id).image
+const shuffle = arr => [...arr].sort(() => Math.random() - 0.5);
 const BASE_URL = 'https://api.openbrewerydb.org/breweries';
 
-const Body = ({search}) =>{
+const Body = ({ search }) => {
 
-  const [brews,setBrews]=useState([]);
+  const [brews, setBrews] = useState([]);
   const [filterBrews, setFilterBrews] = useState([])
 
-  const Tinfo=shuffle((filterBrews.map(({id,name,city,state,phone})=> {return {id,name,city,state,phone}} )));
+  const Tinfo = shuffle((filterBrews.map(({ id, name, city, state, phone }) => { return { id, name, city, state, phone } })));
 
-  const Ids=filterBrews.map(({id},key)=>{
-    let image=key+1;
-    return {id,image:image+'b'}}
+  const Ids = brews.map(({ id }, key) => {
+    if (key >= 20) key = random(19);
+    let image = key + 1;
+    return { id, image: image + 'b' }
+  }
   );
 
-  useEffect(()=>{
+  useEffect(() => {
     const fetchData = async () => {
       const json = await fetch(BASE_URL);
       const data = await json.json();
@@ -25,27 +28,28 @@ const Body = ({search}) =>{
       setFilterBrews(data)
     }
     fetchData().catch(console.error);
-  },[]);
+  }, []);
 
-  useEffect(()=>{
-    const filtered= brews.filter((data)=>{return data.name.toLowerCase().includes(search.toLowerCase())})
-    if(filtered.length===0){
-        setFilterBrews(brews)
-    }else
-    {console.log(filtered)
-    setFilterBrews(filtered)}
-  },[search])
+  useEffect(() => {
+    const filtered = brews.filter((data) => { return data.name.toLowerCase().split(" ").join("").includes(search.toLowerCase().split(" ").join("")) })
+    if (filtered.length === 0) {
+      setFilterBrews(brews)
+    } else {
+      console.log(filtered)
+      setFilterBrews(filtered)
+    }
+  }, [brews, search])
 
 
-  return(
-    Tinfo.map((data,i)=>{
+  return (
+    Tinfo.map((data, i) => {
       return (<Tab
         id={data.id}
         name={data.name}
         city={data.city}
         phone={data.phone}
         key={i}
-        img={chooseImage(Ids,data.id)}
+        img={chooseImage(Ids, data.id)}
       >
       </Tab>);
     })
--- src/Components/Card/SearchBar/SearchBar.js
+++ src/Components/Card/SearchBar/SearchBar.js
@@ -1,23 +1,21 @@
 import './SearchBar.css';
 
-const SearchBar = ({onSearch}) => {
+const SearchBar = ({ onSearch }) => {
 
   const handleKeyDown = e => {
-
-    onSearch(e.target.value)
-
+    if (e.key === 'Enter') onSearch(e.target.value)
   }
 
-  return(
+  return (
     <div className="searchBar">
-     <img src={require("../../../Images/pinterestIcon.svg").default} className="pSign" onClick={()=>{window.location.reload()}} alt="iconP" />
-     <div className="greyWords">Analytics</div>
-     <div className="greyWords">Ads</div>
-     <input className='search' id="input" onChange={handleKeyDown} type="search" placeholder="Search..."/>
-     <div className="greyWords">Home</div>
+      <img src={require("../../../Images/pinterestIcon.svg").default} className="pSign" onClick={() => { window.location.reload() }} alt="iconP" />
+      <div className="greyWords">Analytics</div>
+      <div className="greyWords">Ads</div>
+      <input className='search' id="input" onKeyDown={handleKeyDown} type="search" placeholder="Search..." />
+      <div className="greyWords">Home</div>
     </div>
   )
 
 }
 
-  export default SearchBar;
+export default SearchBar;
